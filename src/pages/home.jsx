b9880e1289d6f597c354247c8592afd37d9bffd2
Edgar Talledos <onimenotsuki@gmail.com>
2020-12-07 12:33:03 -0600

    Agregado useLocation en Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
 import Title from "../components/Title";
 import Banner from "../components/Banner";
 import Form from "../components/FormRecomendados";
 import Footer from "../components/Footer";
 
 const Home = () => {
+  const location = useLocation();
+
+  console.log(location);
+
   return (
     <div className="body">
       <Title />
